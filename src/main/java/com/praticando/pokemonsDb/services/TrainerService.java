3d8e582b3f7740943427803f92c0e68e81b98e4c
Anarquia122 <italogusto823@gmail.com>
2025-03-27 18:13:31 -0300

    Exception handling - delete

--- src/main/java/com/praticando/pokemonsDb/services/TrainerService.java
+++ src/main/java/com/praticando/pokemonsDb/services/TrainerService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.praticando.pokemonsDb.entities.Trainer;
 import com.praticando.pokemonsDb.repositories.TrainerRepository;
+import com.praticando.pokemonsDb.services.exceptions.DatabaseException;
 import com.praticando.pokemonsDb.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class TrainerService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public Trainer update(Long id, Trainer obj) {

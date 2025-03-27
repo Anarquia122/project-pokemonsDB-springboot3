18bf63b4e5adb68d3d538996c13f5665c76786f3
Anarquia122 <italogusto823@gmail.com>
2025-03-27 18:50:42 -0300

    Pokemon delete

--- src/main/java/com/praticando/pokemonsDb/services/PokemonService.java
+++ src/main/java/com/praticando/pokemonsDb/services/PokemonService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.praticando.pokemonsDb.entities.Pokemon;
 import com.praticando.pokemonsDb.repositories.PokemonRepository;
+import com.praticando.pokemonsDb.services.exceptions.DatabaseException;
 import com.praticando.pokemonsDb.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -28,5 +30,15 @@ public class PokemonService {
 	
 	public Pokemon insert(Pokemon obj) {
 		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 }
